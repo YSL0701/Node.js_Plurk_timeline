f3c3b1384840ab33501f1a44f2f47654bb8ce070
YSL0701 <30719leo@gmail.com>
2018-09-11 01:25:34 +0800

    取得user id，之後取得文章內容

--- app.js
+++ app.js
@@ -1,30 +1,50 @@
 var express = require('express')
 var app = express()
 var request = require('request')
+var cheerio = require("cheerio")
 var bodyParser = require('body-parser')
 var jsonParser = bodyParser.json()
 
 app.use(express.static('page'));
 
 
-app.post('/getTimeline',jsonParser,(req,res) => {
+app.post('/getTimeline', jsonParser, (req, res) => {
   console.log(req.body)
-  getTimeline().then((data) => {
+  getUid(req.body.account).then((user_id) => {
+    return getTimeline(user_id)
+  }).then((data) => {
     res.json(data)
   })
 })
 
-app.listen(3000,function(){
+app.listen(3000, function () {
   console.log("sever啟動");
 })
 
-function getTimeline() {
+
+function getUid(account) {
+  return new Promise((resolve, reject) => {
+    request({
+      url: `https://www.plurk.com/${account}`,
+      method: 'GET'
+    }, function(error, response, body) {
+      if(error || !body) {
+        reject();
+      }
+      var $ = cheerio.load(body)
+      var user_id = $('.show_all_friends > a').attr('href').split('?')[1].split('&')[0].split('=')[1]
+      resolve(user_id)
+    })
+  })
+}
+
+function getTimeline(user_id) {
   return new Promise((resolve) => {
     request({
       url: 'https://www.plurk.com/TimeLine/getPlurks',
       method: 'POST',
-      formData: { only_user: 1, user_id: 4686724 }
-    }, function (error, r, body) {
+      formData: { only_user: 1, user_id: user_id }
+    }, function (error, response, body) {
       if (error || !body) { return; }
       // console.log(JSON.parse(b))
       resolve(JSON.parse(body))
